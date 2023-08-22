f9726bb5670417c3599cdc2e599677f23ae9435d
emilalmander <emilalmander@hotmail.se>
2023-08-22 14:28:00 +0000

    deployment commit

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"]="postgres://pydkhkjc:d6VzD6EW-VlzF0ZCAf4NB4PJcDG-2o3K@trumpet.db.elephantsql.com/pydkhkjc"
+os.environ["SECRET_KEY"]="secretKey"
+os.environ["CLOUDINARY_URL"] = "cloudinary://813128856459292:iUTA4-MDjuQ2dHbtPvgamJuTo1E@dybkxrb6a"
